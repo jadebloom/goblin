4a18df6b1c7a26ac49f2eeace9e11ffc10e973c9
jadebloom <warfacemeister@mail.ru>
2026-01-08 20:27:46 -0500

    feat: migrated to angular21 etc.

---
+++ src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Tmp } from './tmp';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Tmp],
+  template: `<gb-tmp />`,
+})
+export class App {}
---
+++ src/app/tmp.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gb-tmp',
+  template: `<p>Tmp</p>`,
+  styles: [
+    `
+      p {
+        color: red;
+      }
+    `,
+  ],
+})
+export class Tmp {}
