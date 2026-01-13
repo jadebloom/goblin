68042e2be2828ad795c9fe759b007b805461e907
jadebloom <warfacemeister@mail.ru>
2026-01-12 22:26:28 -0500

    wip: working reintegrating everything with primeng now

--- project/tasks.md
+++ project/tasks.md
@@ -4,8 +4,6 @@
 
 Version 1.0.0:
 
-- 404 Page.
-- Registration page with logic.
 - Login page with logic.
 - Home page with skeletons.
 - Account page with logic.
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
-import { NotFoundPage } from '@pages/not-found';
+import { NotFoundPage } from '@pages/not-found/not-found-page';
 
 export const routes: Routes = [
+	// {
+	// 	title: 'Goblin. Registration',
+	// 	path: 'registration',
+	// 	component: RegistrationPage,
+	// },
 	{
 		title: 'Goblin. Page not found',
 		path: 'not-found',
