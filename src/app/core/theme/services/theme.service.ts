6eb5314fd7d583bcd34e946a60099023adfa2839
jadebloom <warfacemeister@mail.ru>
2026-01-13 18:15:11 -0500

    feat: added theme change logic

---
+++ src/app/core/theme/services/theme.service.ts
@@ -0,0 +1,52 @@
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
+import { inject, Injectable, PLATFORM_ID } from '@angular/core';
+import { Theme } from '@core/theme/models/theme';
+import { BehaviorSubject, fromEventPattern, map, startWith } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class ThemeService {
+	platformId = inject(PLATFORM_ID);
+
+	private _theme$ = new BehaviorSubject<Theme>(Theme.DARK);
+
+	loadTheme(): Promise<void> {
+		return new Promise(() => {
+			if (isPlatformServer(this.platformId)) {
+				return;
+			}
+
+			const mql = window.matchMedia('(prefers-color-scheme: light)');
+
+			const systemTheme$ = fromEventPattern<MediaQueryList>(
+				(handler) => mql.addEventListener('change', handler),
+				(handler) => mql.removeEventListener('change', handler),
+			).pipe(
+				map((e) => (e.matches ? Theme.LIGHT : Theme.DARK)),
+				startWith(Theme.DARK),
+			);
+
+			systemTheme$.subscribe((theme) => this.setTheme(theme));
+		});
+	}
+
+	toggleTheme() {
+		if (isPlatformBrowser(this.platformId)) {
+			const nextTheme = this._theme$.getValue() == Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+
+			this._theme$.next(nextTheme);
+
+			document.querySelector('html')?.classList.toggle('gb-app-dark');
+		}
+	}
+
+	setTheme(theme: Theme) {
+		const html = document.querySelector('html')?.classList;
+
+		if (html == null) return;
+
+		this._theme$.next(theme);
+
+		if (theme == Theme.DARK) html.add('gb-app-dark');
+		else html.remove('gb-app-dark');
+	}
+}
