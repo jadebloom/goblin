db2a4c6d2cc124a1e192046f2ad26b9cc0d27440
jadebloom <warfacemeister@mail.ru>
2026-01-16 20:50:12 -0500

    wip: working on header and sidebar UI

---
+++ src/app/core/layout/components/nav-sidebar/nav-sidebar.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'gb-layout-nav-sidebar',
+	templateUrl: './nav-sidebar.html',
+	styleUrl: './nav-sidebar.less',
+})
+export class NavSidebar {}
