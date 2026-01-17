bc23daae162fb367902c68254dff7d78d5a4fcc1
jadebloom <warfacemeister@mail.ru>
2026-01-17 15:02:30 -0500

    feat: updated main header UI

--- src/app/toolkit/components/main-header/main-header.ts
+++ src/app/toolkit/components/main-header/main-header.ts
@@ -1,15 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { PopoverModule } from 'primeng/popover';
 import { ButtonModule } from 'primeng/button';
+import { AuthService } from '@core/auth/services/auth.service';
+import { DarkModeToggle } from '@core/theme/components/dark-mode-toggle/dark-mode-toggle';
 import { MainDrawerService } from '@services/main-drawer.service';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'gb-toolkit-main-header',
 	templateUrl: './main-header.html',
-	styleUrl: './main-header.less',
-	imports: [ButtonModule, PopoverModule, RouterLink],
+	imports: [ButtonModule, PopoverModule, DarkModeToggle],
 })
 export class MainHeader {
 	readonly mainDrawer = inject(MainDrawerService);
+	private readonly auth = inject(AuthService);
+	private readonly router = inject(Router);
+
+	readonly popoverItems = [
+		{
+			label: 'Account',
+			icon: 'pi pi-user',
+			onClick: () => this.router.navigate(['/account']),
+		},
+		{
+			label: 'Logout',
+			icon: 'pi pi-sign-out',
+			onClick: this.logout,
+		},
+	];
+
+	logout() {
+		this.auth.logout().subscribe({
+			next: () => {
+				this.router.navigate(['/registration']);
+			},
+		});
+	}
 }
