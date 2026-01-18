133695add2cb8ca42308b106a86cf22af769c28a
jadebloom <warfacemeister@mail.ru>
2026-01-18 17:49:37 -0500

    feat: updated footer

--- src/app/core/layout/components/main-footer/main-footer.ts
+++ src/app/core/layout/components/main-footer/main-footer.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
 	selector: 'gb-layout-main-footer',
 	templateUrl: './main-footer.html',
+	imports: [DividerModule],
 })
 export class MainFooter {}
